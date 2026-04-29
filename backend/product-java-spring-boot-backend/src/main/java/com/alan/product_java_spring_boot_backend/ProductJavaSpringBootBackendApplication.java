72694aecd6f1602859691323b696aa315bfe9644
Alan <140322633+arthenux@users.noreply.github.com>
2026-04-29 02:47:36 +0100

    chore: initialize monorepo structure and secure local config

---
+++ backend/product-java-spring-boot-backend/src/main/java/com/alan/product_java_spring_boot_backend/ProductJavaSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alan.product_java_spring_boot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductJavaSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductJavaSpringBootBackendApplication.class, args);
+	}
+
+}
---
+++ backend/product-java-spring-boot-backend/src/test/java/com/alan/product_java_spring_boot_backend/ProductJavaSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.product_java_spring_boot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductJavaSpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
